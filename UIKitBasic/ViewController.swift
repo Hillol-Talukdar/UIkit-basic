bbdacaece2e74b6578d19ac50e3cd39fee514901
Hillol Talukdar <hillol@inverseai.com>
2024-09-23 08:53:02 +0600

    Initial Commit

---
+++ UIKitBasic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitBasic
+//
+//  Created by Hillol Talukdar on 23/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UIKitBasicTests/UIKitBasicTests.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitBasicTests.swift
+//  UIKitBasicTests
+//
+//  Created by Hillol Talukdar on 23/9/24.
+//
+
+import Testing
+@testable import UIKitBasic
+
+struct UIKitBasicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
